99d967dfb9f8a680a62161153b634636b1e86ab3
Sefinek <contact@sefinek.net>
2025-05-27 17:28:12 +0200

    Update 8.7.4.0

--- DiscordRPC.Example/Program.cs
+++ DiscordRPC.Example/Program.cs
@@ -1,26 +1,27 @@
-﻿using System.Text;
+using System.Text;
+using DiscordRPC;
 using DiscordRPC.IO;
 using DiscordRPC.Logging;
 using DiscordRPC.Message;
 
-namespace DiscordRPC.Example;
+namespace DiscordRpcExample;
 
-internal class Program
+internal static class Program
 {
 	/// <summary>
 	///     The level of logging to use.
 	/// </summary>
-	private static readonly LogLevel logLevel = LogLevel.Trace;
+	private static readonly LogLevel LogLevel = LogLevel.Trace;
 
 	/// <summary>
 	///     The pipe to connect too.
 	/// </summary>
-	private static int discordPipe = -1;
+	private static int _discordPipe = -1;
 
 	/// <summary>
 	///     The current presence to send to discord.
 	/// </summary>
-	private static readonly RichPresence presence = new()
+	private static readonly RichPresence Presence = new()
 	{
 		Details = "Example Project 🎁",
 		State = "csharp example",
@@ -35,21 +36,21 @@ internal class Program
 	/// <summary>
 	///     The discord client
 	/// </summary>
-	private static DiscordRpcClient client;
+	private static DiscordRpcClient _client;
 
 	/// <summary>
 	///     Is the main loop currently running?
 	/// </summary>
-	private static bool isRunning = true;
+	private static bool _isRunning = true;
 
 	/// <summary>
 	///     The string builder for the command
 	/// </summary>
-	private static readonly StringBuilder word = new();
+	private static readonly StringBuilder Word = new();
 
 
-	private static int cursorIndex;
-	private static string previousCommand = "";
+	private static int _cursorIndex;
+	private static string _previousCommand = "";
 
 
 	//Main Loop
@@ -57,12 +58,11 @@ internal class Program
 	{
 		//Reads the arguments for the pipe
 		for (int i = 0; i < args.Length; i++)
-			switch (args[i])
+			_discordPipe = args[i] switch
 			{
-				case "-pipe":
-					discordPipe = int.Parse(args[++i]);
-					break;
-			}
+				"-pipe" => int.Parse(args[++i]),
+				_ => _discordPipe
+			};
 
 		//Seting a random details to test the update rate of the presence
 		//BasicExample();
@@ -79,19 +79,19 @@ internal class Program
 	private static void BasicExample()
 	{
 		// == Create the client
-		DiscordRpcClient client = new("424087019149328395", discordPipe)
+		DiscordRpcClient client = new("424087019149328395", _discordPipe)
 		{
-			Logger = new ConsoleLogger(logLevel, true)
+			Logger = new ConsoleLogger(LogLevel, true)
 		};
 
 		// == Subscribe to some events
-		client.OnReady += (sender, msg) =>
+		client.OnReady += (_, msg) =>
 		{
 			//Create some events so we know things are happening
 			Console.WriteLine("Connected to discord with user {0}", msg.User.Username);
 		};
 
-		client.OnPresenceUpdate += (sender, msg) =>
+		client.OnPresenceUpdate += (_, _) =>
 		{
 			//The presence has updated
 			Console.WriteLine("Presence has been updated! ");
@@ -106,10 +106,10 @@ internal class Program
 			Details = "A Basic Example",
 			State = "In Game",
 			Timestamps = Timestamps.FromTimeSpan(10),
-			Buttons = new[]
-			{
-				new Button { Label = "Fish", Url = "https://lachee.dev/" }
-			}
+			Buttons =
+			[
+				new Button { Label = "HP Multifunction Printer", Url = "https://sefinek.net" }
+			]
 		});
 
 		// == Do the rest of your program.
@@ -124,15 +124,15 @@ internal class Program
 	private static void FullClientExample()
 	{
 		//Create a new DiscordRpcClient. We are filling some of the defaults as examples.
-		using (client = new DiscordRpcClient("424087019149328395", //The client ID of your Discord Application
-			       discordPipe, //The pipe number we can locate discord on. If -1, then we will scan.
-			       new ConsoleLogger(logLevel, true), //The loger to get information back from the client.
+		using (_client = new DiscordRpcClient("424087019149328395", //The client ID of your Discord Application
+			       _discordPipe, //The pipe number we can locate discord on. If -1, then we will scan.
+			       new ConsoleLogger(LogLevel, true), //The loger to get information back from the client.
 			       true, //Should the events be automatically called?
 			       new ManagedNamedPipeClient() //The pipe client to use. Required in mono to be changed.
 		       ))
 		{
 			//If you are going to make use of the Join / Spectate buttons, you are required to register the URI Scheme with the client.
-			client.RegisterUriScheme();
+			_client.RegisterUriScheme();
 
 			//Set the logger. This way we can see the output of the client.
 			//We can set it this way, but doing it directly in the constructor allows for the Register Uri Scheme to be logged too.
@@ -141,26 +141,26 @@ internal class Program
 
 			//Register to the events we care about. We are registering to everyone just to show off the events
 
-			client.OnReady += OnReady; //Called when the client is ready to send presences
-			client.OnClose += OnClose; //Called when connection to discord is lost
-			client.OnError += OnError; //Called when discord has a error
+			_client.OnReady += OnReady; //Called when the client is ready to send presences
+			_client.OnClose += OnClose; //Called when connection to discord is lost
+			_client.OnError += OnError; //Called when discord has a error
 
-			client.OnConnectionEstablished += OnConnectionEstablished; //Called when a pipe connection is made, but not ready
-			client.OnConnectionFailed += OnConnectionFailed; //Called when a pipe connection failed.
+			_client.OnConnectionEstablished += OnConnectionEstablished; //Called when a pipe connection is made, but not ready
+			_client.OnConnectionFailed += OnConnectionFailed; //Called when a pipe connection failed.
 
-			client.OnPresenceUpdate += OnPresenceUpdate; //Called when the presence is updated
+			_client.OnPresenceUpdate += OnPresenceUpdate; //Called when the presence is updated
 
-			client.OnSubscribe += OnSubscribe; //Called when a event is subscribed too
-			client.OnUnsubscribe += OnUnsubscribe; //Called when a event is unsubscribed from.
+			_client.OnSubscribe += OnSubscribe; //Called when a event is subscribed too
+			_client.OnUnsubscribe += OnUnsubscribe; //Called when a event is unsubscribed from.
 
-			client.OnJoin += OnJoin; //Called when the client wishes to join someone else. Requires RegisterUriScheme to be called.
-			client.OnSpectate += OnSpectate; //Called when the client wishes to spectate someone else. Requires RegisterUriScheme to be called.
-			client.OnJoinRequested += OnJoinRequested; //Called when someone else has requested to join this client.
+			_client.OnJoin += OnJoin; //Called when the client wishes to join someone else. Requires RegisterUriScheme to be called.
+			_client.OnSpectate += OnSpectate; //Called when the client wishes to spectate someone else. Requires RegisterUriScheme to be called.
+			_client.OnJoinRequested += OnJoinRequested; //Called when someone else has requested to join this client.
 
 			//Before we send a initial presence, we will generate a random "game ID" for this example.
 			// For a real game, this "game ID" can be a unique ID that your Match Maker / Master Server generates. 
 			// This is used for the Join / Specate feature. This can be ignored if you do not plan to implement that feature.
-			presence.Secrets = new Secrets
+			Presence.Secrets = new Secrets
 			{
 				//These secrets should contain enough data for external clients to be able to know which
 				// game to connect too. A simple approach would be just to use IP address, but this is highly discouraged
@@ -171,7 +171,7 @@ internal class Program
 
 			//We also need to generate a initial party. This is because Join requires the party to be created too.
 			// If no party is set, the join feature will not work and may cause errors within the discord client itself.
-			presence.Party = new Party
+			Presence.Party = new Party
 			{
 				ID = Secrets.CreateFriendlySecret(new Random()),
 				Size = 1,
@@ -180,7 +180,7 @@ internal class Program
 			};
 
 			//Give the game some time so we have a nice countdown
-			presence.Timestamps = new Timestamps
+			Presence.Timestamps = new Timestamps
 			{
 				Start = DateTime.UtcNow,
 				End = DateTime.UtcNow + TimeSpan.FromSeconds(15)
@@ -188,16 +188,16 @@ internal class Program
 
 			//Subscribe to the join / spectate feature.
 			//These require the RegisterURI to be true.
-			client.SetSubscription(EventType.Join | EventType.Spectate | EventType.JoinRequest); //This will alert us if discord wants to join a game
+			_client.SetSubscription(EventType.Join | EventType.Spectate | EventType.JoinRequest); //This will alert us if discord wants to join a game
 
 			//Set some new presence to tell Discord we are in a game.
 			// If the connection is not yet available, this will be queued until a Ready event is called, 
 			// then it will be sent. All messages are queued until Discord is ready to receive them.
-			client.SetPresence(presence);
+			_client.SetPresence(Presence);
 
 			//Initialize the connection. This must be called ONLY once.
 			//It must be called before any updates are sent or received from the discord client.
-			client.Initialize();
+			_client.Initialize();
 
 			//Start our main loop. In a normal game you probably don't have to do this step.
 			// Just make sure you call .Invoke() or some other dequeing event to receive your events.
@@ -213,8 +213,8 @@ internal class Program
 		 * If you were making a GUI application without a infinite loop, you could implement
 		 * this with timers.
 		 */
-		isRunning = true;
-		while (client != null && isRunning)
+		_isRunning = true;
+		while (_client != null && _isRunning)
 		{
 			//We will invoke the client events. 
 			// In a game situation, you would do this in the Update.
@@ -230,7 +230,7 @@ internal class Program
 			//Console.Write("+");
 			Thread.Sleep(25);
 
-			client.SetPresence(presence);
+			_client.SetPresence(Presence);
 		}
 
 		Console.WriteLine("Press any key to terminate");
@@ -246,10 +246,8 @@ internal class Program
 		//  once we finish this method. We only care to get the user info.
 		// If you want to update the presence, dont do this but rather make it a singleton
 		//  that lives throughout the lifetime of your app.
-		using DiscordRpcClient client = new("424087019149328395", discordPipe)
-		{
-			Logger = new ConsoleLogger(logLevel, true)
-		};
+		using DiscordRpcClient client = new("424087019149328395", _discordPipe);
+		client.Logger = new ConsoleLogger(LogLevel, true);
 
 		// == Sub to ready
 		// We are going to listen to the On Ready. Once we have it, we will tell the completion
@@ -273,44 +271,44 @@ internal class Program
 			case ConsoleKey.Enter:
 				//Write the new line
 				Console.WriteLine();
-				cursorIndex = 0;
+				_cursorIndex = 0;
 
 				//The enter key has been sent, so send the message
-				previousCommand = word.ToString();
-				ExecuteCommand(previousCommand);
+				_previousCommand = Word.ToString();
+				ExecuteCommand(_previousCommand);
 
-				word.Clear();
+				Word.Clear();
 				break;
 
 			case ConsoleKey.Backspace:
-				word.Remove(cursorIndex - 1, 1);
+				Word.Remove(_cursorIndex - 1, 1);
 				Console.Write("\r                                         \r");
-				Console.Write(word);
-				cursorIndex--;
+				Console.Write(Word);
+				_cursorIndex--;
 				break;
 
 			case ConsoleKey.Delete:
-				if (cursorIndex < word.Length)
+				if (_cursorIndex < Word.Length)
 				{
-					word.Remove(cursorIndex, 1);
+					Word.Remove(_cursorIndex, 1);
 					Console.Write("\r                                         \r");
-					Console.Write(word);
+					Console.Write(Word);
 				}
 
 				break;
 
 			case ConsoleKey.LeftArrow:
-				cursorIndex--;
+				_cursorIndex--;
 				break;
 
 			case ConsoleKey.RightArrow:
-				cursorIndex++;
+				_cursorIndex++;
 				break;
 
 			case ConsoleKey.UpArrow:
-				word.Clear().Append(previousCommand);
+				Word.Clear().Append(_previousCommand);
 				Console.Write("\r                                         \r");
-				Console.Write(word);
+				Console.Write(Word);
 				break;
 
 			default:
@@ -318,18 +316,18 @@ internal class Program
 				{
 					//Some other character key was sent
 					Console.Write(key.KeyChar);
-					word.Insert(cursorIndex, key.KeyChar);
+					Word.Insert(_cursorIndex, key.KeyChar);
 					Console.Write("\r                                         \r");
-					Console.Write(word);
-					cursorIndex++;
+					Console.Write(Word);
+					_cursorIndex++;
 				}
 
 				break;
 		}
 
-		if (cursorIndex < 0) cursorIndex = 0;
-		if (cursorIndex >= Console.BufferWidth) cursorIndex = Console.BufferWidth - 1;
-		Console.SetCursorPosition(cursorIndex, Console.CursorTop);
+		if (_cursorIndex < 0) _cursorIndex = 0;
+		if (_cursorIndex >= Console.BufferWidth) _cursorIndex = Console.BufferWidth - 1;
+		Console.SetCursorPosition(_cursorIndex, Console.CursorTop);
 	}
 
 	private static void ExecuteCommand(string word)
@@ -345,29 +343,29 @@ internal class Program
 		int whitespaceIndex = word.IndexOf(' ');
 		if (whitespaceIndex >= 0)
 		{
-			command = word.Substring(0, whitespaceIndex);
+			command = word[..whitespaceIndex];
 			if (whitespaceIndex < word.Length)
-				body = word.Substring(whitespaceIndex + 1);
+				body = word[(whitespaceIndex + 1)..];
 		}
 
 		//Parse the command
 		switch (command.ToLowerInvariant())
 		{
 			case "close":
-				client.Dispose();
+				_client.Dispose();
 				break;
 
 			#region State & Details
 
 			case "state":
 				//presence.State = body;
-				presence.State = body;
-				client.SetPresence(presence);
+				Presence.State = body;
+				_client.SetPresence(Presence);
 				break;
 
 			case "details":
-				presence.Details = body;
-				client.SetPresence(presence);
+				Presence.Details = body;
+				_client.SetPresence(Presence);
 				break;
 
 			#endregion
@@ -376,42 +374,42 @@ internal class Program
 
 			case "large_key":
 				//If we do not have a asset object already, we must create it
-				if (!presence.HasAssets())
-					presence.Assets = new Assets();
+				if (!Presence.HasAssets())
+					Presence.Assets = new Assets();
 
 				//Set the key then send it away
-				presence.Assets.LargeImageKey = body;
-				client.SetPresence(presence);
+				Presence.Assets.LargeImageKey = body;
+				_client.SetPresence(Presence);
 				break;
 
 			case "large_text":
 				//If we do not have a asset object already, we must create it
-				if (!presence.HasAssets())
-					presence.Assets = new Assets();
+				if (!Presence.HasAssets())
+					Presence.Assets = new Assets();
 
 				//Set the key then send it away
-				presence.Assets.LargeImageText = body;
-				client.SetPresence(presence);
+				Presence.Assets.LargeImageText = body;
+				_client.SetPresence(Presence);
 				break;
 
 			case "small_key":
 				//If we do not have a asset object already, we must create it
-				if (!presence.HasAssets())
-					presence.Assets = new Assets();
+				if (!Presence.HasAssets())
+					Presence.Assets = new Assets();
 
 				//Set the key then send it away
-				presence.Assets.SmallImageKey = body;
-				client.SetPresence(presence);
+				Presence.Assets.SmallImageKey = body;
+				_client.SetPresence(Presence);
 				break;
 
 			case "small_text":
 				//If we do not have a asset object already, we must create it
-				if (!presence.HasAssets())
-					presence.Assets = new Assets();
+				if (!Presence.HasAssets())
+					Presence.Assets = new Assets();
 
 				//Set the key then send it away
-				presence.Assets.SmallImageText = body;
-				client.SetPresence(presence);
+				Presence.Assets.SmallImageText = body;
+				_client.SetPresence(Presence);
 				break;
 
 			#endregion
@@ -436,7 +434,7 @@ internal class Program
 		// It will give us some basic information about discord to use in the future.
 
 		//DEBUG: Update the presence timestamp
-		presence.Timestamps = Timestamps.Now;
+		Presence.Timestamps = Timestamps.Now;
 
 		//It can be a good idea to send a inital presence update on this event too, just to setup the inital game state.
 		Console.WriteLine("On Ready. RPC Version: {0}", args.Version);
@@ -471,7 +469,7 @@ internal class Program
 		//This is called when the client fails to establish a connection to discord. 
 		// It can be assumed that Discord is unavailable on the supplied pipe.
 		Console.WriteLine("Pipe Connection Failed. Could not connect to pipe #{0}", args.FailedPipe);
-		isRunning = false;
+		_isRunning = false;
 	}
 
 	#endregion
